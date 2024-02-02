1b549490f56a4e532df6f389eb59d7a545501267
Jörg Finkeisen <joerg@finkeisen.net>
2024-02-02 14:57:34 +0100

    add cocktails

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-// HINTS:
-// 1. Import express and axios
 import express from "express";
 import axios from "axios";
-// 2. Create an express app and set the port number.
+
 const app = express();
 const port = 3000;
 const jokeAPI_URL = "https://api.chucknorris.io/jokes/random";
+const cocktailAPI_URL = "http://www.thecocktaildb.com/api/json/v1/1/random.php";
 
-// 3. Use the public folder for static files.
 app.use(express.static("public"));
 
-// 4. When the user goes to the home page it should render the index.ejs file.
-// 5. Use axios to get a random secret and pass it to index.ejs to display the
-// secret and the username of the secret.
 app.get("/", async (req, res) => {
     try {
-      const result = await axios.get(API_URL);
+      const dayJoke = await axios.get(jokeAPI_URL);
+      const dayCocktail = await axios.get(cocktailAPI_URL);
+      console.log(dayCocktail.data.drinks[0])
       res.render("index.ejs", {
-        joke: result.data.value,
-        pic: result.data.icon_url
+        joke : dayJoke.data.value,
+        cocktail : dayCocktail.data.drinks[0].strDrink,
+        receipt : dayCocktail.data.drinks[0].strInstructions
     });
     } catch (error) {
       console.error("Failed to make request:", error.message);
@@ -26,10 +24,8 @@ app.get("/", async (req, res) => {
         error: error.message,
       });
     }
-  });
+});
 
-
-// 6. Listen on your predefined port and start the server.
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
